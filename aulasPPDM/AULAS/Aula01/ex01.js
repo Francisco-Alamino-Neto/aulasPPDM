3e1e258c5c35bf7189edb785f17e60150c3ab374
Francisco Alamino Neto <franciscoalaminoneto26@gmail.com>
2026-03-12 04:41:08 -0300

    Delete aulasPPDM directory

--- aulasPPDM/AULAS/Aula01/ex01.js
+++
@@ -1,9 +0,0 @@
-let nota = prompt("Informe uma nota: ");
-
-if(nota >= 7){
-    console.log("Aprovado");
-}else if(nota < 5){
-    console.log("Reprovado");
-} else {
-    console.log("Recuperação");
-}
